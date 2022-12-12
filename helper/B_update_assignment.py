db17718c5e3188fd16889b684832c7d1fb9d2e8c
Black-Frost <vovantiendung2001@gmail.com>
2022-12-12 20:29:40 +0700

    [F2] Fix xpath

--- helper/B_update_assignment.py
+++ helper/B_update_assignment.py
@@ -2,10 +2,10 @@ from helper.base import DDT_edge, handle_datetime
 from selenium.webdriver.common.by import By
 
 class B_update_assignment(DDT_edge):
-    def click_add_file_button(self, xpath):
+    def click_add_file_button(self):
         while True:
             try:
-                button_add = self.find_ele(By.XPATH, xpath)
+                button_add = self.find_ele(By.XPATH, "//a[@title=\"Thêm...\"]")
                 self.click(button_add)
                 break
             except:
@@ -14,7 +14,7 @@ class B_update_assignment(DDT_edge):
     def fill_file_path(self, xpath, file_path):
         while True:
             try:
-                file_form = self.find_ele(By.XPATH, xpath)
+                file_form = self.find_ele(By.XPATH, "//input[@name=\"repo_upload_file\"]")
                 self.text(file_form, file_path)
                 break
             except:
@@ -23,7 +23,7 @@ class B_update_assignment(DDT_edge):
     def fill_name_form(self, xpath, name):
         while True:
             try:
-                name_form = self.find_ele(By.XPATH, xpath)
+                name_form = self.find_ele(By.XPATH, "//input[@class=\"form-control\"][@name=\"title\"]")
                 self.text(name_form, name)
                 break
             except:
@@ -32,7 +32,7 @@ class B_update_assignment(DDT_edge):
     def click_upload(self, xpath):
         while True:
             try:
-                button = self.find_ele(By.XPATH, xpath)
+                button = self.find_ele(By.XPATH, "//*[@class=\"fp-upload-btn btn-primary btn\"]")
                 self.click(button)
                 break
             except:
@@ -40,7 +40,7 @@ class B_update_assignment(DDT_edge):
 
     def check_if_success(self, xpath):
         try:
-            error = self.find_ele(By.XPATH, xpath)
+            error = self.find_ele(By.XPATH, "//h5[text()='Lỗi']")
             return False
         except:
             return True
